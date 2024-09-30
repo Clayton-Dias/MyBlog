59da11e1c7e7160f80e8783cb5ebd28ea819de49
Clayton-Dias <crad13@hotmail.com>
2024-09-30 16:54:48 -0300

    2024-09-30
    Usando SQL com Flask

--- database/myblogdb.sql
+++ database/myblogdb.sql
@@ -8,7 +8,7 @@ DROP DATABASE IF EXISTS myblogdb;
 
 -- Cria o banco de dados novamente
 -- PERIGO! Não faça isso em modo de produção
-CREATE DATABASE jocablogdb
+CREATE DATABASE myblogdb
     -- Usando o conjunto de caracteres UTF-8
     CHARACTER SET utf8mb4
     -- Buscas em UTF-8 e case insensitive
@@ -136,21 +136,24 @@ INSERT INTO staff(
 
 -- Tabela "article"
 INSERT INTO article(
+    art_date,           -- Data do artigo
     art_title,          -- Título do artigo
     art_resume,         -- Resumo do artigo
     art_thumbnail,      -- URL da imagem thumbnail do artigo
     art_content,        -- Conteúdo completo do artigo
     art_author          -- ID do autor (referência para staff)
 ) VALUES (
+    -- Gera uma data aleatória entre '2024-01-01 00:00:00' e '2024-12-31 23:59:59'
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Primeiro Artigo',
     'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
-    'https://picsum.photos/id/1/200/300',
+    'https://picsum.photos/id/1/300',
     '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima iure ratione molestiae modi soluta aperiam dicta
     repudiandae esse maxime dolor at possimus quidem fugiat, velit nam asperiores? Soluta, obcaecati pariatur.</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus animi commodi praesentium perspiciatis sit totam,
     ullam aliquam voluptatum veniam nostrum facere sapiente nesciunt repellat eligendi labore deserunt eos officiis
     dolor.</p>
-    <img src="https://picsum.photos/id/1/200/300" alt="Imagem Aleatória">
+    <img src="https://picsum.photos/id/1/300" alt="Imagem Aleatória">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolores ut incidunt minus vel debitis quod asperiores
     maxime, aut corrupti doloremque saepe quasi vitae perspiciatis temporibus soluta earum dolore voluptate?</p>
     <h4>Lista de links</h4>
@@ -164,63 +167,73 @@ INSERT INTO article(
 ),
 -- Inserções pelo chatGPT
 (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Segundo Artigo',  -- Título do segundo artigo
     'Explorando a natureza e suas belezas',  -- Resumo
-    'https://picsum.photos/id/2/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/2/300',  -- Thumbnail do artigo
     '<p>A natureza é um dos maiores presentes que temos. Neste artigo, exploramos suas maravilhas e o que podemos fazer para preservá-las.</p>',  -- Conteúdo
     '1'  -- Autor: Cinval dos Santos
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Terceiro Artigo',  -- Título do terceiro artigo
     'Tecnologia e Inovação',  -- Resumo
-    'https://picsum.photos/id/3/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/3/300',  -- Thumbnail do artigo
     '<p>A tecnologia está mudando rapidamente a forma como vivemos. Vamos discutir algumas inovações que estão moldando nosso futuro.</p>',  -- Conteúdo
     '5'  -- Autor: Roberto Silva
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Quarto Artigo',  -- Título do quarto artigo
     'Dicas de leitura',  -- Resumo
-    'https://picsum.photos/id/4/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/4/300',  -- Thumbnail do artigo
     '<p>Se você é um amante de livros, confira nossas dicas de leitura que vão ampliar seus horizontes e enriquecer seu conhecimento.</p>',  -- Conteúdo
     '2'  -- Autor: Ana Carolina Ribeiro
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Quinto Artigo',  -- Título do quinto artigo
     'Cozinhando com amor',  -- Resumo
-    'https://picsum.photos/id/5/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/5/300',  -- Thumbnail do artigo
     '<p>Cozinhar é uma arte. Neste artigo, falamos sobre receitas que trazem o amor e a tradição para a mesa.</p>',  -- Conteúdo
     '6'  -- Autor: Juliana Costa
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Sexto Artigo',  -- Título do sexto artigo
     'Viajando pelo mundo',  -- Resumo
-    'https://picsum.photos/id/6/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/6/300',  -- Thumbnail do artigo
     '<p>Viajar é uma forma de aprender sobre diferentes culturas. Aqui estão algumas dicas para sua próxima aventura.</p>',  -- Conteúdo
     '4'  -- Autor: Mariana Ferreira
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Sétimo Artigo',  -- Título do sétimo artigo
     'A importância do autocuidado',  -- Resumo
-    'https://picsum.photos/id/7/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/7/300',  -- Thumbnail do artigo
     '<p>O autocuidado é fundamental para a nossa saúde mental. Vamos explorar práticas que podem ajudar.</p>',  -- Conteúdo
     '3'  -- Autor: Lucas Almeida
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Oitavo Artigo',  -- Título do oitavo artigo
     'O futuro da educação',  -- Resumo
-    'https://picsum.photos/id/8/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/8/300',  -- Thumbnail do artigo
     '<p>A educação está em constante evolução. Neste artigo, discutimos o que o futuro nos reserva nesse aspecto.</p>',  -- Conteúdo
     '2'  -- Autor: Ana Carolina Ribeiro
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Nono Artigo',  -- Título do nono artigo
     'Sustentabilidade no dia a dia',  -- Resumo
-    'https://picsum.photos/id/9/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/9/300',  -- Thumbnail do artigo
     '<p>Viver de forma sustentável é uma responsabilidade de todos. Veja como pequenas mudanças podem fazer a diferença.</p>',  -- Conteúdo
     '5'  -- Autor: Roberto Silva
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Décimo Artigo',  -- Título do décimo artigo
     'Criatividade e inovação',  -- Resumo
-    'https://picsum.photos/id/10/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/10/300',  -- Thumbnail do artigo
     '<p>Ser criativo é essencial em todos os aspectos da vida. Vamos explorar como a criatividade pode impulsionar a inovação.</p>',  -- Conteúdo
     '6'  -- Autor: Juliana Costa
 ), (
+    FROM_UNIXTIME( UNIX_TIMESTAMP('2024-01-01 00:00:00') + FLOOR(RAND() * (UNIX_TIMESTAMP('2024-12-31 23:59:59') - UNIX_TIMESTAMP('2024-01-01 00:00:00'))) ),
     'Décimo Primeiro Artigo',  -- Título do décimo primeiro artigo
     'Cuidado com a saúde mental',  -- Resumo
-    'https://picsum.photos/id/11/200/300',  -- Thumbnail do artigo
+    'https://picsum.photos/id/11/300',  -- Thumbnail do artigo
     '<p>A saúde mental é tão importante quanto a saúde física. Aqui estão algumas dicas para cuidar do seu bem-estar emocional.</p>',  -- Conteúdo
     '3'  -- Autor: Lucas Almeida
 );
@@ -270,126 +283,3 @@ INSERT INTO comment(
     'eduardolima@email.com',  -- Email do autor do comentário
     'O autocuidado é fundamental! Muito importante falar sobre isso.'  -- Conteúdo do comentário
 );
-
-
-/*** Otimizado e comentado pelo chat GPT 
-
--- --------------------------- 
--- Banco de dados "myblogdb" 
--- --------------------------- 
-
--- Apaga o banco de dados caso ele já exista
--- PERIGO! Não faça isso em modo de produção
-DROP DATABASE IF EXISTS myblogdb;
-
--- Cria o banco de dados
--- !!!!!!!!!!!!!!!!!!!!!! ATENÇÃO !!!!!!!!!!!!!!!!!!!
--- Remova este comando após a publicação da versão de produção
-CREATE DATABASE myblogdb 
-    CHARACTER SET utf8mb4 
-    COLLATE utf8mb4_general_ci;
-
--- Seleciona o banco de dados para os próximos comandos
-USE myblogdb;
-
--- Criação da tabela "staff" para armazenar informações dos usuários do blog
-CREATE TABLE staff (
-    sta_id INT PRIMARY KEY AUTO_INCREMENT, -- ID único do staff
-    sta_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data de criação do registro
-    sta_name VARCHAR(127) NOT NULL, -- Nome do staff
-    sta_email VARCHAR(255) NOT NULL UNIQUE, -- Email do staff (único)
-    sta_password VARCHAR(63) NOT NULL, -- Senha do staff (armazenada como hash)
-    sta_birth DATE NOT NULL, -- Data de nascimento
-    sta_description VARCHAR(255), -- Descrição do staff
-    sta_type ENUM('moderator', 'author', 'admin') DEFAULT 'moderator', -- Tipo de usuário
-    sta_status ENUM('on', 'off', 'del') DEFAULT 'on' -- Status do usuário
-);
-
--- Criação da tabela "article" para armazenar os artigos do blog
-CREATE TABLE article (
-    art_id INT PRIMARY KEY AUTO_INCREMENT, -- ID único do artigo
-    art_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data de criação do artigo
-    art_title VARCHAR(127) NOT NULL, -- Título do artigo
-    art_resume VARCHAR(255) NOT NULL, -- Resumo do artigo
-    art_thumbnail VARCHAR(255) NOT NULL, -- Thumbnail do artigo
-    art_content TEXT NOT NULL, -- Conteúdo do artigo
-    art_view INT DEFAULT 0, -- Contador de visualizações
-    art_author INT, -- ID do autor (referência para staff)
-    art_status ENUM('on', 'off', 'del') DEFAULT 'on', -- Status do artigo
-    FOREIGN KEY (art_author) REFERENCES staff (sta_id) -- Chave estrangeira para staff
-);
-
--- Criação da tabela "comment" para armazenar os comentários nos artigos
-CREATE TABLE comment (
-    com_id INT PRIMARY KEY AUTO_INCREMENT, -- ID único do comentário
-    com_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data de criação do comentário
-    com_article INT, -- ID do artigo (referência para article)
-    com_author_name VARCHAR(127) NOT NULL, -- Nome do autor do comentário
-    com_author_email VARCHAR(127) NOT NULL, -- Email do autor do comentário
-    com_comment TEXT NOT NULL, -- Conteúdo do comentário
-    com_status ENUM('on', 'off', 'del') DEFAULT 'on', -- Status do comentário
-    FOREIGN KEY(com_article) REFERENCES article(art_id) -- Chave estrangeira para article
-);
-
--- Criação da tabela "contact" para armazenar mensagens de contato
-CREATE TABLE contact (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- ID único da mensagem
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data de criação da mensagem
-    name VARCHAR(127) NOT NULL, -- Nome do remetente
-    email VARCHAR(127) NOT NULL, -- Email do remetente
-    subject VARCHAR(255) NOT NULL, -- Assunto da mensagem
-    message TEXT NOT NULL, -- Conteúdo da mensagem
-    status ENUM('received', 'readed', 'responded', 'deleted') DEFAULT 'received' -- Status da mensagem
-);
-
--- ------------------------------------------- 
--- Popula(colocar) as tabelas com alguns dados 
--- ------------------------------------------- 
-
--- Tabela "staff"
-INSERT INTO staff (sta_name, sta_email, sta_password, sta_birth, sta_description, sta_type) VALUES 
-    ('Cinval dos Santos', 'cincin@email.com', SHA1('a1b2c3'), '2002-06-15', 'De tudo mais um pouco', 'admin'),
-    ('Ana Carolina Ribeiro', 'anari@email.com', SHA1('qwert9513'), '1990-01-25', 'Professora, leitora, curiosa e exploradora', 'author'),
-    ('Lucas Almeida', 'lucas.almeida@email.com', SHA1('password123'), '1985-03-10', 'Aventureiro e amante da natureza', 'moderator'),
-    ('Mariana Ferreira', 'mariana.ferreira@email.com', SHA1('mariana456'), '1995-07-20', 'Escritora e viajante', 'author'),
-    ('Roberto Silva', 'roberto.silva@email.com', SHA1('roberto789'), '1978-11-05', 'Entusiasta da tecnologia e inovação', 'admin'),
-    ('Juliana Costa', 'juliana.costa@email.com', SHA1('juliana321'), '1992-02-15', 'Designer e criadora de conteúdo', 'author');
-
--- Tabela "article"
-INSERT INTO article (art_title, art_resume, art_thumbnail, art_content, art_author) VALUES 
-    ('Primeiro Artigo', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit', 'https://picsum.photos/id/1/200/300',
-     '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>', 2), -- Simplificado para brevidade
-    ('Segundo Artigo', 'Explorando a natureza e suas belezas', 'https://picsum.photos/id/2/200/300',
-     '<p>A natureza é um dos maiores presentes que temos...</p>', 1),
-    ('Terceiro Artigo', 'Tecnologia e Inovação', 'https://picsum.photos/id/3/200/300',
-     '<p>A tecnologia está mudando rapidamente a forma como vivemos...</p>', 5),
-    ('Quarto Artigo', 'Dicas de leitura', 'https://picsum.photos/id/4/200/300',
-     '<p>Se você é um amante de livros, confira nossas dicas...</p>', 2),
-    ('Quinto Artigo', 'Cozinhando com amor', 'https://picsum.photos/id/5/200/300',
-     '<p>Cozinhar é uma arte...</p>', 6),
-    ('Sexto Artigo', 'Viajando pelo mundo', 'https://picsum.photos/id/6/200/300',
-     '<p>Viajar é uma forma de aprender sobre diferentes culturas...</p>', 4),
-    ('Sétimo Artigo', 'A importância do autocuidado', 'https://picsum.photos/id/7/200/300',
-     '<p>O autocuidado é fundamental para a nossa saúde mental...</p>', 3),
-    ('Oitavo Artigo', 'O futuro da educação', 'https://picsum.photos/id/8/200/300',
-     '<p>A educação está em constante evolução...</p>', 2),
-    ('Nono Artigo', 'Sustentabilidade no dia a dia', 'https://picsum.photos/id/9/200/300',
-     '<p>Viver de forma sustentável é uma responsabilidade de todos...</p>', 5),
-    ('Décimo Artigo', 'Criatividade e inovação', 'https://picsum.photos/id/10/200/300',
-     '<p>Ser criativo é essencial em todos os aspectos da vida...</p>', 6),
-    ('Décimo Primeiro Artigo', 'Cuidado com a saúde mental', 'https://picsum.photos/id/11/200/300',
-     '<p>A saúde mental é tão importante quanto a saúde física...</p>', 3);
-
--- Tabela "comment"
-INSERT INTO comment (com_article, com_author_name, com_author_email, com_comment) VALUES 
-    (2, 'Rivaldinho Almeida', 'rivaldinho_al@email.com', 'Lorem ipsum dolor sit amet...'),
-    (1, 'Maria Clara', 'mariaclara@email.com', 'Adorei o artigo! Muito informativo e bem escrito.'),
-    (3, 'João Pedro', 'joaopedro@email.com', 'Ótimas dicas de tecnologia! Mal posso esperar para ver as inovações futuras.'),
-    (4, 'Ana Beatriz', 'anabeatriz@email.com', 'Essas dicas de leitura são incríveis!'),
-    (5, 'Carlos Silva', 'carlossilva@email.com', 'Amei a receita! Com certeza vou experimentar.'),
-    (6, 'Fernanda Souza', 'fernandasouza@email.com', 'Viajar é tudo de bom! Obrigada pelas dicas.'),
-    (7, 'Eduardo Lima', 'eduardolima@email.com', 'O autocuidado é fundamental! Muito importante falar sobre isso.');
-
-
-
-***/
--- index.py
+++ index.py
@@ -1,23 +1,69 @@
-from flask import Flask, render_template
 # Importação do Flask: Aqui estamos importando a classe Flask do pacote Flask, que é um microframework para construção de aplicações web em Python.
+from flask import Flask, render_template
+from flask_mysqldb import MySQL, MySQLdb
 
 # Constantes do site
-SITE ={
-    "NAME" : "MyBlog",
-    "OWNER" : "Meu Blog",
-    "LOGO": "/static/img/icone.png",
+SITE = {
+    "NAME": "MyBlog",
+    "OWNER": "Meu Blog",
+    "LOGO": "/static/img/icone.png",   
 }
 
-app = Flask(__name__)
+
 # Inicialização da aplicação: Criamos uma instância da classe Flask. O argumento __name__ ajuda o Flask a determinar o caminho da aplicação e a localizar recursos como templates e arquivos estáticos.
+app = Flask(__name__)
+
+# Configurações de acesso ao MySQL
+'''
+app.config['MYSQL_HOST'] = 'localhost'  # Servidor do MySQL
+app.config['MYSQL_USER'] = 'root'       # Usuário do MySQL
+app.config['MYSQL_PASSWORD'] = ''       # Senha do MySQL
+app.config['MYSQL_DB'] = 'myblogdb'     # Nome da base de dados
+'''
+app.config.update(
+    MYSQL_HOST='localhost',       # Servidor do MySQL
+    MYSQL_USER='root',            # Usuário do MySQL
+    MYSQL_PASSWORD='',            # Senha do MySQL
+    MYSQL_DB='myblogdb'           # Nome da base de dados
+)
+
+# Variável de conexão com o MySQL
+mysql = MySQL(app)
+
 
 # @ -> anotation
-
 # Definição de rota: O decorador @app.route('/') define uma rota para a URL raiz (/). A função home() será chamada quando alguém acessar essa URL.
 
 # Rota para a página inicial → raiz
 @app.route('/')
 def home():
+
+    # Consulta SQL
+    sql = '''
+        -- Seleciona os campos art_id, art_title, art_resume e art_thumbnail da tabela article
+        SELECT art_id, art_title, art_resume, art_thumbnail
+
+        -- Define a tabela de onde os dados serão extraídos
+        FROM article
+
+        -- Filtra os resultados para incluir apenas artigos que estão "on" (ativos) e cuja data é anterior ou igual à data atual
+        WHERE art_status = 'on'  -- Verifica se o status do artigo é 'on'
+        AND art_date <= NOW()    -- Compara a data do artigo com a data e hora atuais
+
+        -- Ordena os resultados pela data do artigo em ordem decrescente (do mais recente para o mais antigo)
+        ORDER BY art_date DESC;
+    '''
+
+    # Executa a query e obtém os dados na forma de DICT
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute(sql)
+    articles = cur.fetchall()
+    cur.close()
+
+    # Somente para debug
+    # Funciona somente quando a route para a raiz for acionada
+    # print('\n\n\n', articles, '\n\n\n')
+
     # Passa parâmetros para o template
     # "CSS" e "JS" são opcionais
     toPage = {
@@ -27,18 +73,71 @@ def home():
         "js": "home.js",            # JavaScript desta página (opcional)
 
         # Outras chaves usadas pela página
-
+        'articles': articles
     }
     # Renderiza template passando a variável local `toPage`
     # para o template como `page`.
     return render_template("home.html", page=toPage)
+
+
+@app.route('/view/<artid>')
+def view(artid):
+
+    if not artid.isdigit():
+        return page_not_found(404)
+
+    sql = '''
+        SELECT art_id, art_date, art_title, art_content,
+
+        -- Obtém a data em PT_BR pelo pseudo-campo 'art_datebr'
+        DATE_FORMAT(art_date, '%%d/%%m/%%Y às %%H:%%i') AS art_datebr,
+        sta_id, sta_name, sta_image, sta_description,
+
+        -- Calcula a idade para 'sta_age' considerando ano, mês e dia de nascimento
+        TIMESTAMPDIFF(YEAR, sta_birth, CURDATE()) - 
+        (DATE_FORMAT(CURDATE(), '%%m%%d') < DATE_FORMAT(sta_birth, '%%m%%d')) AS sta_age
+        
+        FROM article 
+
+        -- Realiza um INNER JOIN entre a tabela article e a tabela staff
+        INNER JOIN staff ON art_author = sta_id
+        WHERE art_id = %s  -- Filtra para obter apenas o artigo com ID específico
+            AND art_status = 'on'  -- Considera apenas artigos que estão ativos
+            AND art_date <= NOW();  -- Inclui apenas artigos cuja data é menor ou igual à data atual
+
+    '''
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute(sql, (artid,))
+    article = cur.fetchone()
+
+    # Para debug
+    # print('\n\n\n', article, '\n\n\n')
+
+    if article is None:
+        return page_not_found(404)
+    
+    sql = 'UPDATE article SET art_view = art_view +1  WHERE art_id = %s'
+    cur.execute(sql, (artid,))
+    mysql.connection.commit()
+
+
+    toPage={
+        'site' : SITE,
+        'title': article["art_title"],
+        'css':'view.css',
+        'article': article
+    }
+
+    cur.close()
+    return render_template("view.html", page=toPage)
+
 
 # Rota para a página dec contatos → /contacts
 @app.route('/contacts')
 def contacts():
     page = {
         "site": SITE,
-        "title": "Faça contatos",        
+        "title": "Faça contatos",
         "css": "home.css",
     }
     return render_template("contacts.html", page=page)
@@ -49,10 +148,20 @@ def contacts():
 def about():
     page = {
         "site": SITE,
-        "title": "Sobre",       
+        "title": "Sobre",
         "css": "about.css"
     }
     return render_template("about.html", page=page)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    toPage = {
+        'title': 'Erro 404',
+        'site': SITE,
+        'css': '404.css'
+    }
+    return render_template('404.html', page=toPage), 404
 
 
 # Verificação de execução: Este bloco garante que o código dentro dele só será executado se o script for executado diretamente, e não se for importado como um módulo em outro script.
